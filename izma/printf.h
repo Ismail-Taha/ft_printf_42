24d198646c64edc93c8c36caa097169d4084a510
Ismail-Taha <taha.ismaile2002@gmail.com>
2024-12-04 23:15:47 +0100

    norm up

--- izma/printf.h
+++ izma/printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   printf.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isallali <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: isallali <isallali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/04 22:31:22 by isallali          #+#    #+#             */
-/*   Updated: 2024/12/04 22:32:10 by isallali         ###   ########.fr       */
+/*   Created: 2024/12/04 22:51:00 by isallali          #+#    #+#             */
+/*   Updated: 2024/12/04 23:10:04 by isallali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,46 +21,42 @@
 /* Flags structure */
 typedef struct s_flags
 {
-    int space;
-    int hash;
-    int plus;
-    int	index;
-    int	ret;
-} t_flags;
+	int	space;
+	int	hash;
+	int	plus;
+	int	index;
+	int	ret;
+}	t_flags;
 
 typedef struct s_tool
 {
-	int	num;
+	int				num;
 	unsigned int	n;
 	unsigned int	count;
-	char	buffer[12];
-	int	i;
-}   t_tool;
+	char			buffer[12];
+	int				i;
+}	t_tool;
 
-/* helpers */
 char	base_to_char(unsigned long num, char c);
-int	ft_puts(char c);
-int	ft_putstring(char *str);
+int		ft_putc(char c);
+int		ft_putstrr(char *str);
 
-/* printers */
 int		ft_putchar(va_list args, t_flags *flags);
 int		ft_putstr(va_list args, t_flags *flags);
 int		ft_putnbr(va_list args, t_flags *flags);
 int		ft_putnbr_unsigned(va_list args, t_flags *flags);
 int		ft_puthexa(va_list args, t_flags *flags);
 int		ft_puthe16a(va_list args, t_flags *flags);
-int		ft_putpointer(va_list args, t_flags *flags);
+int		ft_putptr(va_list args, t_flags *flags);
 
-/* struct to hold functions */
 typedef struct functions_holder
 {
 	char	symbol;
 	int		(*puts)(va_list arg, t_flags *flags);
 }	t_holder;
 
-/* main functions */
 int		ft_printf(const char *format, ...);
-int		ft_looper(const char *format, t_holder *printers,
+int		process(const char *format, t_holder *printers,
 			va_list args, int *count);
 
-#endif
+#endif /*PRINTF_H*/
