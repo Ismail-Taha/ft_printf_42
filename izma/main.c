2f341eea3e198d4694b24a9752f1b523921f18ad
Ismail-Taha <taha.ismaile2002@gmail.com>
2024-12-05 22:41:06 +0100

    knt anzleg f put_ptr

--- izma/main.c
+++ izma/main.c
@@ -5,8 +5,9 @@
 
 int main(void) {
 
-    ft_printf("\n%d\n",ft_printf("vb%%nm%", 1));
-	printf("\n%d\n",printf("vb%%nm%", 1));
+    void *ptr;
+    ft_printf("\n%d\n",ft_printf("%                +p", ptr));
+	printf("\n%d\n",printf("%                +p", ptr));
     /*char *str = "abc% s\n%            d\n%##  pdef\n";
     ft_printf("\n%d\n",ft_printf(str, "Hello World", INT_MAX, str));
 	printf("\n%d\n",printf(str, "Hello World", INT_MAX, str));*/
